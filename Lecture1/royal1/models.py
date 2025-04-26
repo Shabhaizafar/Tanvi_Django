58fae8c1813b28c7efeb6676b4603181f16b9f75
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-26 14:32:03 +0530

    Models

--- Lecture1/royal1/models.py
+++ Lecture1/royal1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Royal(models.Model):
+    fname = models.CharField(max_length=255)
+    age = models.IntegerField(max_length=100)
+
