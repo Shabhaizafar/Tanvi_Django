91c5201956cc0d2e03c5913c067c1544c5fdccbe
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-19 12:40:42 +0530

    Models Completed

--- Lecture1/royal1/models.py
+++ Lecture1/royal1/models.py
@@ -5,4 +5,14 @@ from django.db import models
 class Royal(models.Model):
     fname = models.CharField(max_length=255)
     age = models.IntegerField(max_length=100)
+    lname = models.CharField(null=True)
 
+'''
+[ 
+{'id': 1, 'fname': 'Raj', 'age': 28}, 
+{'id': 2, 'fname': 'Ajay', 'age': 29}, 
+{'id': 3, 'fname': 'Rajesh', 'age': 40}, 
+{'id': 4, 'fname': 'Vivek', 'age': 21}, 
+{'id': 5, 'fname': 'Rajesh', 'age': 30}
+]
+'''
