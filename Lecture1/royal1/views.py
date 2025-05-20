4f24602f0e974d196d1432fc441ca1463fa74241
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-20 12:35:14 +0530

    Display Data in a Webpage

--- Lecture1/royal1/views.py
+++ Lecture1/royal1/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Royal
+from django.template import loader
 # Create your views here.
 
 
@@ -8,11 +10,18 @@ def royal1(self):
 
 
 
-def home(request):
+# def home(request):
     # mypage = loader.get_template('firstData.html')
     # return HttpResponse(mypage.render())
-    return render(request, 'firstData.html')
+    # return render(request, 'firstData.html')
 
+def home(request):
+    allusers = Royal.objects.all().values()
+    mytemp = loader.get_template('user.html')
+    context  = {
+        'allusers' : allusers
+    }
+    return HttpResponse(mytemp.render(context,request))
 
 def about(request):
     return render(request, 'About.html')
