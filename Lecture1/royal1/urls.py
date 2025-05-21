cfa8d43d627756530f6bfe9f0a983ffd1ba88560
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-21 12:47:40 +0530

    Add Links with Templates

--- Lecture1/royal1/urls.py
+++ Lecture1/royal1/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('royal1/',views.royal1,name='royal1'),
     path('home/',views.home,name='home'),
+    path('home/linksdetails/<int:id>',views.linksdetails,name="linksdetails"),
     path('about/',views.about,name='about'),
 ]
+
--- Lecture1/royal1/views.py
+++ Lecture1/royal1/views.py
@@ -15,13 +15,31 @@ def royal1(self):
     # return HttpResponse(mypage.render())
     # return render(request, 'firstData.html')
 
+# def home(request):
+#     allusers = Royal.objects.all().values()
+#     mytemp = loader.get_template('user.html')
+#     context  = {
+#         'allusers' : allusers
+#     }
+#     return HttpResponse(mytemp.render(context,request))
+
 def home(request):
     allusers = Royal.objects.all().values()
-    mytemp = loader.get_template('user.html')
+    mytemp = loader.get_template('allusers.html')
     context  = {
         'allusers' : allusers
     }
     return HttpResponse(mytemp.render(context,request))
+
+
+
+def linksdetails(request,id):
+    user = Royal.objects.get(id=id)
+    mtemp1 = loader.get_template('linksdetails.html')
+    context = {
+        'user' : user
+    }
+    return HttpResponse(mtemp1.render(context,request))
 
 def about(request):
     return render(request, 'About.html')
